71e4c3515683d25515763e837f433699511b7a53
unknown <jamie.connell_10@hotmail.com>
2024-04-11 21:25:03 +0100

    (following walkalong videos)

---
+++ my-app/src/components/LifeCyclesCDU.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import LifeCyclesCDUChild from './LifeCyclesCDUChild'
+
+export class LifeCyclesCDU extends Component {
+    constructor(props) {
+
+        super(props)
+        this.state = {
+            greetings: 'Hello!'
+        }
+    }
+
+    updateGreeting = () => {
+        console.log('update greeting called')
+        this.setState(prevState => {
+            return {
+                greeting: prevState.greeting === 'Hello!' ? 'Goodbye': 'Hello!'
+            }
+        })
+    }
+
+    componentDidUpdate() {
+        console.log('Component updated')
+    }
+
+    render() {
+        console.log('render method called')
+        return (
+            <div>
+                <h1>{this.state.greeting}</h1>
+                <button onClick={this.updateGreeting}>Update Greeting</button>
+                <LifeCyclesCDUChild />
+            </div>
+        )
+    }
+}
+
+export default LifeCyclesCDU
